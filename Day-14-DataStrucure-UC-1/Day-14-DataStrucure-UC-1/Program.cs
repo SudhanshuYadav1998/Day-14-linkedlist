44513c9e2955ddd1904d6c04329d37d24166e50f
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-27 21:03:42 +0530

    Linkedlist UC-1 files Added

---
+++ Day-14-DataStrucure-UC-1/Day-14-DataStrucure-UC-1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+public class LinkedListProblem
+{
+    internal Node head;
+    internal void Add(int data)
+    {
+        Node node = new Node(data);
+        if (this.head == null)
+        {
+            this.head = node;
+        }
+        else
+        {
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+        Console.WriteLine("{0} inserted into linked list", node.data);
+
+        }
+    }
+
+
+internal class Node
+{
+    public int data;
+    public Node next;
+
+    public Node(int data)
+    { this.data = data; }
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedListProblem list = new LinkedListProblem();
+        list.Add(56);
+        list.Add(30);
+        list.Add(70);
+    }
+}
